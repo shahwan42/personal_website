ee8ba949056d414a9fc829b60fe4407553ce63e9
Ahmed Shahwan <ahmed@shahwan.me>
2020-05-01 22:00:01 +0200

    pages app

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,6 +75,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "pp.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    "pp.pages.apps.PagesConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
